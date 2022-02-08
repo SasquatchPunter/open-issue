7ec337cbf80262abaabeb2fc6b4734b15e41680c
Jeremy Elliott <jodomoi64@yahoo.ca>
2022-02-08 15:17:25 -0700

    feat: port issue action

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const wait = require('./wait');
-const process = require('process');
-const cp = require('child_process');
-const path = require('path');
+//const process = require('process');
+//const cp = require('child_process');
+//const path = require('path');
 
 test('throws invalid number', async () => {
   await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
@@ -16,9 +16,9 @@ test('wait 500 ms', async () => {
 });
 
 // shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 100;
-  const ip = path.join(__dirname, 'index.js');
-  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
-  console.log(result);
-})
+// test('test runs', () => {
+//   process.env['INPUT_MILLISECONDS'] = 100;
+//   const ip = path.join(__dirname, 'index.js');
+//   const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+//   console.log(result);
+// })
